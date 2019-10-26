f66e4177938f3b708cba393ae6c53c0bd83e3654
Attila Szita <atee88@gmail.com>
2019-10-26 16:05:31 +0200

    simplify, cleanup

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,14 @@
-import { mazeOptions, NewMaze, MazeData, Directions } from "../types"
+import { mazeOptions, NewMaze, MazeData, Directions } from '../types'
 
 const METHOD = {
   GET: 'GET',
   POST: 'POST'
 }
 
-export const createOptions = ({ method = METHOD.GET, body = null }: { method?: string, body?: mazeOptions } = {}): RequestInit => ({
+export const createOptions = ({
+  method = METHOD.GET,
+  body = null
+}: { method?: string; body?: mazeOptions } = {}): RequestInit => ({
   method,
   headers: {
     Accept: 'application/json',
@@ -14,33 +17,43 @@ export const createOptions = ({ method = METHOD.GET, body = null }: { method?: s
   body: JSON.stringify(body)
 })
 
-const status = async (res: Response) => {
+const status = (res: Response) => {
   if (!res.ok) {
     throw res
   }
   return res.json()
 }
 
-export function createMaze({ width = 15, height = 15, name = 'Twilight Sparkle', difficulty = 0 }): Promise<NewMaze> {
-  return fetch("https://ponychallenge.trustpilot.com/pony-challenge/maze", createOptions({
-    method: 'POST',
-    body: {
-      "maze-width": width,
-      "maze-height": height,
-      "maze-player-name": name,
-      "difficulty": difficulty
-    }
-  })).then(status)
+export function createMaze({
+  width = 15,
+  height = 15,
+  name = 'Twilight Sparkle',
+  difficulty = 0
+}): Promise<NewMaze> {
+  return fetch(
+    'https://ponychallenge.trustpilot.com/pony-challenge/maze',
+    createOptions({
+      method: 'POST',
+      body: {
+        'maze-width': width,
+        'maze-height': height,
+        'maze-player-name': name,
+        difficulty: difficulty
+      }
+    })
+  ).then(status)
 }
 
-export async function getMazeById(id: string): Promise<MazeData> {
+export function getMazeById(id: string): Promise<MazeData> {
   return fetch(`https://ponychallenge.trustpilot.com/pony-challenge/maze/${id}`).then(status)
-
 }
 
-export async function sendMove(id: string, direction: Directions) {
-  return fetch(`https://ponychallenge.trustpilot.com/pony-challenge/maze/${id}`, createOptions({
-    method: METHOD.POST,
-    body: { direction }
-  })).then(status)
+export function sendMove(id: string, direction: Directions) {
+  return fetch(
+    `https://ponychallenge.trustpilot.com/pony-challenge/maze/${id}`,
+    createOptions({
+      method: METHOD.POST,
+      body: { direction }
+    })
+  ).then(status)
 }
--- src/hooks/move.ts
+++ src/hooks/move.ts
@@ -1,34 +1,30 @@
 import React, { Dispatch } from 'react'
 import { isArrowKey } from '../api/utils'
 import * as api from '../api'
-import { AppState, Actions, Directions } from '../types'
+import { AppState, Actions } from '../types'
 import { ACTIONS, arrowDirections } from '../constants'
 
 export const useMove = (state: AppState, update: Dispatch<Actions>) => {
-  const [keyPressed, setKeyPressed] = React.useState('')
-
-  const movePony = React.useCallback(async (mazeId: string, move: Directions) => {
-    try {
-      await api.sendMove(mazeId, move)
-      const mazeUpdate = await api.getMazeById(mazeId)
-      update({ type: ACTIONS.UPDATE_MAZE, payload: mazeUpdate })
-    } catch (e) {
-      console.log(e)
-      update({ type: ACTIONS.MOVE, payload: move })
-    }
-  }, [update])
-
-  React.useEffect(() => {
-    const setKey = ({ key }: KeyboardEvent) => setKeyPressed(key)
-    window.addEventListener('keydown', setKey)
-    return () => window.removeEventListener('keydown', setKey)
-  }, [])
+  const movePony = React.useCallback(
+    async (mazeId: string, keyPressed) => {
+      if (isArrowKey(keyPressed)) {
+        const nextMove = arrowDirections[keyPressed]
+        try {
+          await api.sendMove(mazeId, nextMove)
+          const mazeUpdate = await api.getMazeById(mazeId)
+          update({ type: ACTIONS.UPDATE_MAZE, payload: mazeUpdate })
+        } catch (e) {
+          console.log(e)
+          update({ type: ACTIONS.MOVE, payload: nextMove })
+        }
+      }
+    },
+    [update]
+  )
 
   React.useEffect(() => {
-    if (isArrowKey(keyPressed)) {
-      const nextMove = arrowDirections[keyPressed]
-      setKeyPressed('')
-      movePony(state.mazeId, nextMove)
-    }
-  }, [keyPressed, movePony, state.mazeId, update])
+    const move = ({ key }: KeyboardEvent) => movePony(state.mazeId, key)
+    window.addEventListener('keydown', move)
+    return () => window.removeEventListener('keydown', move)
+  }, [movePony, state.mazeId])
 }
